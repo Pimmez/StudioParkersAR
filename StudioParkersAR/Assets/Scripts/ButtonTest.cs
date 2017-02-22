d31f2d41be76107a256bcc455887385794b5e687
Pim Horeman <pimhoreman@gmail.com>
2017-02-22 16:55:29 +0100

    Camera Focus!
    -Added Camera Focus
    -Back Button on Android
    -Ingame informationText added
    -Visit Cards work now!
    -Added Fonts
    -Minor Bug Fixes

---
+++ StudioParkersAR/Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour {
+
+    [SerializeField] private string gameName, mainMenu;
+
+    private bool loadScene = false;
+    [SerializeField]
+    GameObject buttonGame, buttonUrl, mainText;
+
+    [SerializeField]
+    private Text loadingText;
+
+    private void Start()
+    {
+        buttonGame.SetActive(true);
+        mainText.SetActive(true);
+        buttonUrl.SetActive(true);
+        loadingText.enabled = false;
+        Quit();
+    }
+
+
+    // Updates once per frame
+    void Update()
+    {
+
+        // If the new scene has started loading...
+        if (loadScene == true)
+        {
+            // ...then pulse the transparency of the loading text to let the player know that the computer is still working.
+            loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b, Mathf.PingPong(Time.time, 1));
+         }
+
+        Quit();
+
+    }
+
+    void Quit()
+    {
+        if (Application.platform == RuntimePlatform.Android && Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().name == gameName)
+            {
+                SceneManager.LoadScene(mainMenu);
+            }
+            else
+            {
+                //Stops The Game
+                Application.Quit();
+            }
+            
+        }
+    }
+
+    public void ToGame()
+    {
+        if (loadScene == false)
+        {
+            buttonGame.SetActive(false);
+            mainText.SetActive(false);
+            buttonUrl.SetActive(false);
+            loadingText.enabled = true;
+
+            // ...set the loadScene boolean to true to prevent loading a new scene more than once...
+            loadScene = true;
+
+            // ...change the instruction text to read "Loading..."
+            loadingText.text = "Loading...";
+
+            // ...and start a coroutine that will load the desired scene.
+            StartCoroutine(LoadNewScene());
+        }
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+
+    public void OpenURL()
+    {
+        #if UNITY_EDITOR
+            Application.OpenURL("https://www.studioparkers.nl");
+        #elif (UNITY_ANDROID || UNITY_IPHONE || UNITY_WP8)
+            Application.OpenURL("https://www.studioparkers.nl");
+        #endif
+    } 
+
+    IEnumerator LoadNewScene()
+    {
+        // This line waits for 3 seconds before executing the next line in the coroutine.
+        yield return new WaitForSeconds(3);
+
+        // Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
+        AsyncOperation aSync = SceneManager.LoadSceneAsync(gameName);
+
+        // While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
+        while (!aSync.isDone)
+        {
+            yield return null;
+        }
+
+    }
+
+}
---
+++ StudioParkersAR/Assets/Scripts/InformationText.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InformationText : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject panelText, infoText;
+
+    [SerializeField]
+    private string tagName;
+
+    //Set the gameobjects invisible
+    private void Start()
+    {
+        panelText.SetActive(false);
+        infoText.SetActive(false);
+    }
+
+    //The moment they collide with each other
+    void OnTriggerEnter(Collider other)
+    {
+        //If the tag of this object is equel to the string, set the gameobjects to visible
+        if(this.gameObject.tag == tagName)
+        {
+            panelText.SetActive(true);
+            infoText.SetActive(true);
+        }
+    }
+
+    //The moment they don't collide with each other
+    void OnTriggerExit(Collider other)
+    {
+        if (this.gameObject.tag == tagName)
+        {
+            panelText.SetActive(false);
+            infoText.SetActive(false);
+        }
+    }
+}
